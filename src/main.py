8ed2ae52ddc0e527de0223d660451de1f23d2db6
bugmaker <z1015947658@gmail.com>
2023-08-15 01:21:05 +0800

    Added visualization base module

---
+++ src/main.py
@@ -0,0 +1,10 @@
+
+from visualizer import Visualizer
+
+if __name__ == '__main__':
+    visualizer = Visualizer("C:/Users/1015947658/Desktop/rangenet-pytorch/src/config/visualizer.yaml")
+    visualizer.run()   
+    
+    
+    
+     
